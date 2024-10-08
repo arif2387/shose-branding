417b0b9f0a9cf45c299f36e6b1103340fa41408d
Adesh Chaturvedi <adeshchaturvedi51@gmail.com>
2024-10-08 12:51:06 +0530

    here i fix code in your navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Navigation = () => {
   return (
     <div>
-      <nav className='container' >
-   < div className='logo' >
-   <img src="images\nike.jpg"  alt="logo"  />
-
-   </div>
-     <ul>
-      <li href="#" >Menu</li>
-      <li href="#" >Location</li>
-      <li href="#" >About</li>
-      <li href="#" >More</li>
-     </ul>
-     <button> Login</button>
-     </nav>
+      <nav className="container">
+        <div className="logo">
+          <img src="images\nike.jpg" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="https://www.nike.com/in/">More</li>
+        </ul>
+        <button> Login</button>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
